280aa84957bb97355fbc8d4b4e23197391718345
Nazar Kruk <nazarkruk@icloud.com>
2022-01-18 22:04:46 -0500

    Done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solutions in this file
+let messeges = []  
+function writeCards(names, eventName){
+    
+    for (let i = 0; i < names.length; i++){
+       
+        messeges.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`)
+        
+    }
+
+    return messeges
+    
+}
+
+function countDown(num) {
+    while (num>=0){
+        console.log(num)
+        num--
+        
+    } 
+}
+countDown(10)
+
